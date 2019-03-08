fe613270a5baaae8aa8dd61d834c338dfcf9f436
agrimgrover <agrover@>
2019-03-08 10:39:48 -0500

    demo

---
+++ src/main/java/com/workfusion/lang/LangDetect.java
@@ -0,0 +1,28 @@
+package com.workfusion.lang;
+
+public class LangDetect {
+
+	public static void main(String[] args) {
+		LangDetect l = new LangDetect();
+		try {
+			l.detect();
+		}
+		catch (Exception ex) {
+			System.out.println("Unspecified error from agrimgrover" + ex);
+		}
+	}
+
+
+	private void detect() throws Exception {
+		LangDetectSample lds = new LangDetectSample();
+		System.out.println("I am here");
+		lds.init(
+			"/Users/agrimgrover/Documents/disk/data/workspace/gitclone/github/language-detector-1/src/main/resources/com/cybozu/labs/langdetect/profiles/defaultprofile");
+		System.out.println(lds.detect("This is sample text"));
+		System.out.println(lds.detect("Maître Corbeau, sur un arbre perché,"));
+		System.out.println(lds.detect("Как вас зовут?"));
+		System.out.println(lds.detect("王明：这是什么？"));
+		System.out.println(lds.detect("Jeder hat das Recht auf Bildung. Die Bildung ist unentgeltlich, zum mindesten der Grundschulunterricht und die grundlegende Bildung. Der Grundschulunterricht ist obligatorisch. Fach- und Berufsschulunterricht müssen allgemein verfügbar gemacht werden, und der Hochschulunterricht muß allen gleichermaßen entsprechend ihren Fähigkeiten offenstehen."));
+	}
+
+}
---
+++ src/main/java/com/workfusion/lang/LangDetectSample.java
@@ -0,0 +1,37 @@
+package com.workfusion.lang;
+
+/**
+ * agrimgrover should have written a comment here.
+ *
+ * @author agrimgrover
+ */
+
+import java.util.ArrayList;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+import com.cybozu.labs.langdetect.Language;
+
+
+public class LangDetectSample {
+
+	public void init(String profileDirectory) throws LangDetectException {
+		DetectorFactory.loadProfile(profileDirectory);
+	}
+
+
+	public String detect(String text) throws LangDetectException {
+		Detector detector = DetectorFactory.create();
+		detector.append(text);
+		return detector.detect();
+	}
+
+
+	public ArrayList<Language> detectLangs(String text) throws LangDetectException {
+		Detector detector = DetectorFactory.create();
+		detector.append(text);
+		return detector.getProbabilities();
+	}
+
+}
